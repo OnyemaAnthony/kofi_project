76647b24c6f615faed6b627fe019b736708440a5
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-07-13 16:00:10 +0100

    video upload WIP

---
+++ kofi_project/lib/screens/confirm_screen.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+import '../widgets/text_input_field.dart';
+
+class ConfirmScreen extends StatefulWidget {
+  final File videoFile;
+  final String videoPath;
+  const ConfirmScreen({
+    Key? key,
+    required this.videoFile,
+    required this.videoPath,
+  }) : super(key: key);
+
+  @override
+  State<ConfirmScreen> createState() => _ConfirmScreenState();
+}
+
+class _ConfirmScreenState extends State<ConfirmScreen> {
+  late VideoPlayerController controller;
+  TextEditingController _songController = TextEditingController();
+  TextEditingController _captionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      controller = VideoPlayerController.file(widget.videoFile);
+    });
+    controller.initialize();
+    controller.play();
+    controller.setVolume(1);
+    controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.5,
+              child: VideoPlayer(controller),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: TextInputField(
+                      controller: _songController,
+                      labelText: 'Name of video',
+                      icon: Icons.abc_sharp,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    width: MediaQuery.of(context).size.width - 20,
+                    child: TextInputField(
+                      controller: _captionController,
+                      labelText: 'Caption',
+                      icon: Icons.closed_caption,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                        
+                        shape:
+                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                    onPressed: (){
+
+                    },
+                      child: const Text(
+                        'Share!',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
